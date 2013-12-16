ff8bd3f088ee7ac8d7e8c677afc43991e138e5a7
Henry Wang <wh_henry@hotmail.com>
2013-12-17 10:53:17 +1100

    added comments

--- emacstts/emacstts.py
+++ emacstts/emacstts.py
@@ -1,81 +1,112 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2013-  Henry Wang <wh_henry@hotmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import pyttsx
+import logging
 from epc.server import *
 
-##
-# TTS class
-##
+
+### TTS class ###
 class EmacsTTS:
+    """A pyttsx wrapper class that provides convenient interface to clients"""
+
     def __init__(self):
+        """Construct the inner pyttsx engine and setup the callbacks"""
         self._eng = pyttsx.init()
         self._eng.connect("started-utterance", self._onStart)
         self._eng.connect("started-word", self._onWord)
         self._eng.connect("finished-utterance", self._onEnd)
 
     def _prepare(self):
+        """Helper method that make sure the engine loop is terminated"""
         try:
             self._eng.endLoop()
         except:
             pass
 
-    def say(self, text):
-        print "say..."
-        self._prepare()
-        self._eng.say(text)
-        self._eng.startLoop(False)
+    def _flush(self):
+        """Helper method that flushes the engine's command queue"""
         for x in xrange(0,10):
             self._eng.iterate()
 
+    def say(self, text):
+        """Request the engine to speak supplied text string"""
+        logging.debug("say...")
+        self._prepare()
+        self._eng.say(text)
+        self._eng.startLoop(False)
+        self._flush()
+
     def shutup(self):
-        print "shutup..."
+        """Ask the engine to stop speaking"""
+        logging.debug("shutup...")
         self._eng.stop()
 
     def louder(self):
+        """Increase the voice's volume"""
         self._prepare()
         vol = self._eng.getProperty("volume")
         newvol = vol + 0.25
-        print "louder %f => %f" %(vol, newvol)
+        logging.debug("louder %f => %f" %(vol, newvol))
         self._eng.setProperty("volume", newvol)
         self._eng.runAndWait()
         self.say("louder")
 
     def quieter(self):
+        """Reduce the voice's volume"""
         self._prepare()
         vol = self._eng.getProperty("volume")
         newvol = vol - 0.25
-        print "quieter %f => %f" %(vol, newvol)
+        logging.debug("quieter %f => %f" %(vol, newvol))
         self._eng.setProperty("volume", newvol)
         self._eng.runAndWait()
         self.say("quieter")
 
     def faster(self):
+        """Increase the speaking speed (number of words per minute)"""
         self._prepare()
         rate = self._eng.getProperty("rate")
         newrate = rate + 50
-        print "faster %d => %d" %(rate, newrate)
+        logging.debug("faster %d => %d" %(rate, newrate))
         self._eng.setProperty("rate", newrate)
         self._eng.runAndWait()
         self.say("faster")
 
     def slower(self):
+        """Reduce the speaking speed (number of words per minute)"""
         self._prepare()
         rate = self._eng.getProperty("rate")
         newrate = rate - 50
-        print "slower %d => %d" %(rate, newrate)
+        logging.debug("slower %d => %d" %(rate, newrate))
         self._eng.setProperty("rate", newrate)
         self._eng.runAndWait()
         self.say("slower")
 
     def voices(self):
-        print "voices..."
+        """Return a list of voices available on the system"""
+        logging.debug("voices...")
         v = []
         for voice in self._eng.getProperty("voices"):
             v.append(voice.name)
         return v
 
     def setvoice(self, name):
-        print "setvoice..."
+        """Set the current voice to the supplied person's name"""
+        logging.debug("setvoice...")
         for voice in self._eng.getProperty("voices"):
             if voice.name == name:
                 self._prepare()
@@ -85,57 +116,65 @@ class EmacsTTS:
                 break
 
     def _onStart(self, name):
-        print "onStart..."
+        """Callback method when a utterance is spoken"""
+        logging.debug("onStart...")
 
     def _onWord(self, name, location, length):
-        print "onWord..."
+        """Callback method when a word is spoken"""
+        logging.debug("onWord...")
 
     def _onEnd(self, name, completed):
-        print "onEnd..."
+        """Callback method when a utterance is finished spoken"""
+        logging.debug("onEnd...")
 
 
-##
-# Public interfaces
-##
+### Public interfaces ###
 ttsEng = EmacsTTS()
 
 def say(*args):
+    """Request the engine to speak given text"""
     try:
         ttsEng.say(args[0])
     except e, Exception:
         print e
 
 def shutup():
+    """Ask the engine to stop speaking"""
     try:
         ttsEng.shutup()
     except e, Exception:
         print e
 
 def louder():
+    """Increase the voice's volume"""
     try:
         ttsEng.louder()
     except e, Exception:
         print e
 
 def quieter():
+    """Reduce the voice's volume"""
     try:
         ttsEng.quieter()
     except e, Exception:
         print e
 
 def faster():
+    """Increase the speaking rate (words/min)"""
     try:
         ttsEng.faster()
     except e, Exception:
         print e
 
 def slower():
+    """Reduce the speaking rate (words/min)"""
     try:
         ttsEng.slower()
     except e, Exception:
         print e
 
 def voices():
+    """Return a list of voices avaiable on the system"""
     try:
         return ttsEng.voices()
     except e, Exception:
@@ -143,15 +182,14 @@ def voices():
     return ""
 
 def setvoice(*args):
+    """Set the current voice to the give person"""
     try:
         ttsEng.setvoice(args[0])
     except e, Exception:
         print e
 
 
-##
-# Main Program
-##
+### Main Program ###
 if __name__ == "__main__":
     server = EPCServer(("localhost", 5085))
     server.register_function(say)
--- emacstts/testeng.py
+++ emacstts/testeng.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2013-  Henry Wang <wh_henry@hotmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import emacstts
 import cmd
 import pyttsx
@@ -13,44 +28,55 @@ else:
 
 
 class TestCmd(cmd.Cmd):
+    """A console for test command input"""
 
     def __init__(self):
+        """Construct the console"""
         cmd.Cmd.__init__(self)
         self.prompt = ">>> "
         self._voices = [ v for v in emacstts.voices() ]
 
     def do_say(self, args):
+        """Test the speaking function"""
         emacstts.say(args)
 
     def do_shutup(self, args):
-        print "shutup"
+        """Test the stopping function"""
         emacstts.shutup()
 
     def do_voices(self, args):
+        """Test the voice listing function"""
         for v in self._voices:
             print v,
         print
 
     def do_setvoice(self, args):
+        """Test the voice setting function"""
         if args in self._voices:
             emacstts.setvoice(args)
 
     def do_louder(self, args):
+        """Test the volume setting"""
         emacstts.louder()
 
     def do_quieter(self, args):
+        """Test the volume setting"""
         emacstts.quieter()
 
     def do_faster(self, args):
+        """Test the rate setting"""
         emacstts.faster()
 
     def do_slower(self, args):
+        """Test the rate setting"""
         emacstts.slower()
 
     def do_EOF(self, line):
+        """Exit the console"""
         return True
 
     def complete_setvoice(self, text, line, begidx, endidx):
+        """Voice candidate completions"""
         if not text:
             rval = self._voices[:]
         else:
